9ab43cbf80e2d7da5c5f1d5c3638c311a242dac4
Ryan <mailforabe7@yahoo.com>
2021-04-05 13:56:16 +0200

    Creates a database and console logs names

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const router = express.Router();
-
+const sqlite3 = require("sqlite3").verbose();
 
 
 //Set the view engine to the one we have
@@ -11,13 +11,77 @@ app.set("view engine", "ejs");
 
 //I think this is for setting up the 'path'usages
 app.use('/', router);
+
 //serve static files in express
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.urlencoded({ extended: false })); // use the middleware “express.urlencoded()” so that request.body retrieves the posted values
 
-// Determine what is returned upon a request. Now we render index.ejs
+// Connection to the SQlite database
+const db_name = path.join(__dirname, "data", "apptest.db");
+console.log("Database full path - " + db_name);
+const db = new sqlite3.Database(db_name, (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log("Successful connection to the database 'apptest.db'");
+});
+
+
+// Creating user table (userID, Name, Surname, Date Vaccinated (TBI), Type of Vaccine (TBI), Location )
+const sql_create = `CREATE TABLE IF NOT EXISTS User (
+    User_ID INTEGER PRIMARY KEY AUTOINCREMENT,
+    Name VARCHAR(100) NOT NULL,
+    Surname VARCHAR(100) NOT NULL,
+    Location VARCHAR(100) NOT NULL
+  );`;
+
+
+//This just runs sql code to make a new table
+db.run(sql_create, (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log("Successful creation of the 'User' table")});
+  
+// Creates string for sql command
+const sql_insert = `INSERT INTO User (Name, Surname, Location) VALUES
+    ('Ryan','Davies','Claremont'),
+    ('Garviel','Loken', 'Cadia');`;
+
+//Runs sql string. 
+db.run(sql_insert, (err) => {
+      if (err) {
+        return console.error(err.message);
+      }
+      console.log("Successful creation of 2 users");
+    });
+
+
+
+
+
+
+
+// Used to render index.ejs and use database info
 router.get("/", function (req, res) {
-      res.render(__dirname + "/views/index.ejs");
+    const sql = "SELECT * FROM User";
+    db.all(sql, [], (err, rows) => {
+      if (err) {
+        return console.error(err.message);
+      }
+
+      //Just playing around with this for debugging purposes
+      rows.forEach((row) => {
+        console.log(row.Name);
+      });
+  
+      console.log("rows - " + rows.length);
+      //__dirname resolves to your project folder.
+      res.render(__dirname + "/views/index.ejs", {
+        status: "No status",
+        users: rows,
+      });
+    });
   });
 
 

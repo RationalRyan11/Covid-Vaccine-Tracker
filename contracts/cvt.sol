b7d4f2bc174411183829d35ccf8ebaea658d0f0f
Ryan <mailforabe7@yahoo.com>
2021-05-21 14:21:44 +0200

    Last commit for this part

--- contracts/cvt.sol
+++ contracts/cvt.sol
@@ -1,8 +1,9 @@
 pragma solidity ^0.5.0;
+pragma experimental ABIEncoderV2;
 
+//Contract made for this assignment
 contract covidVaccineRegister {
-
-  // define the struct Day1User
+  //VaccinationRecord structure used
   struct VaccinationRecord {
     string Name;
     string Surname;
@@ -11,27 +12,31 @@ contract covidVaccineRegister {
     string VaccinationPlace;
   }
 
-  // define the array of users
+  //This array will store all of the records of people taking the vaccine
   VaccinationRecord[] public vaccinationRecords;
 
+  //Inserts 1 entry into the record using the vaccinationRecords structure
   function createVaccinationRecord(string calldata _Name, string calldata _Surname, string calldata _vaccineDate, string calldata _vaccineName, string calldata _vaccinePlace) external returns(uint){
-    // get an instance of a Day1User using the input variables and push into the array of day1_users, returns the id
+
     uint id = vaccinationRecords.push(VaccinationRecord(_Name, _Surname, _vaccineDate, _vaccineName, _vaccinePlace )) - 1;
-    
-    // return the user id
     return id;
   }
 
+  //Simply returns the number of vaccination records
   function getNumberOfVaccinations() external view returns(uint) {
-    // return the length of the day1_users array
+
     return vaccinationRecords.length;
   }
 
-  function getNumberOfVaccinations2() external view returns(uint) {
-    // return the length of the day1_users array
-    return vaccinationRecords.length;
+  //Returns a record
+  function getVaccinationRecord(uint x) external view returns(VaccinationRecord memory) {
+    VaccinationRecord memory record = vaccinationRecords[x];
+    return record;
   }
 
   
 
 }
+
+//covidVaccineRegister.deployed().then(function(instance) { app = instance })
+//createVaccinationRecord("Ryan","Davies","2021-01-01","Johnson","Claremont")
